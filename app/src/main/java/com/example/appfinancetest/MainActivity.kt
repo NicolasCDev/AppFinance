0db1137c808c46f19a170b0402f7b5661912f6cf
NicolasCDev <nicolas.chauveau03@gmail.com>
2025-04-06 11:16:57 +0200

    Base layer

--- app/src/main/java/com/example/appfinancetest/MainActivity.kt
+++ app/src/main/java/com/example/appfinancetest/MainActivity.kt
@@ -1,33 +1,138 @@
 package com.example.appfinancetest
 
+import android.app.Application
+import android.util.Log
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import androidx.activity.compose.setContent// Import correcte pour l'extension
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
 import com.example.appfinancetest.ui.theme.AppFinanceTestTheme
+import androidx.compose.runtime.mutableStateListOf
+import androidx.room.*
+import android.content.Context
+import androidx.activity.viewModels
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AppFinanceTestTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    var selectedItem by remember { mutableIntStateOf(0) }
+    val items = listOf("Tableau de bord", "Investissement", "Patrimoine", "DataBase")
+    val icons = listOf(R.drawable.ic_dashboard, R.drawable.ic_investment, R.drawable.ic_patrimoine, R.drawable.ic_database)
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar {
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        icon = {
+                            Icon(
+                                painter = painterResource(id = icons[index]),
+                                contentDescription = item
+                            )
+                        },
+                        label = { Text(item) },
+                        selected = selectedItem == index,
+                        onClick = { selectedItem = index }
                     )
                 }
             }
         }
+    ) { innerPadding ->
+        when (selectedItem) {
+            0 -> DashboardScreen(modifier = Modifier.padding(innerPadding))
+            1 -> InvestissementScreen(modifier = Modifier.padding(innerPadding))
+            2 -> PatrimoineScreen(modifier = Modifier.padding(innerPadding))
+            3 -> DataBaseScreen(modifier = Modifier.padding(innerPadding))
+            else -> {
+                ErrorScreen(modifier = Modifier.padding(innerPadding))
+            }
+        }
     }
+}
+
+@Composable
+fun DashboardScreen(modifier: Modifier = Modifier) {
+    Surface(modifier = modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Greeting(name = "Tableau de bord")
+        }
+    }
+}
+
+@Composable
+fun InvestissementScreen(modifier: Modifier = Modifier) {
+    Surface(modifier = modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Greeting(name = "Investissement")
+        }
+    }
+}
+
+@Composable
+fun PatrimoineScreen(modifier: Modifier = Modifier) {
+    Surface(modifier = modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Greeting(name = "Patrimoine")
+        }
+    }
+}
+
+@Composable
+fun DataBaseScreen(modifier: Modifier = Modifier) {
+    Surface(modifier = modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Greeting(name = "Database")
+        }
+    }
+}
+
+@Composable
+fun ErrorScreen(modifier: Modifier = Modifier){
+    Greeting(name = "Error")
 }
 
 @Composable
@@ -36,12 +141,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppFinanceTestTheme {
-        Greeting("Android")
-    }
 }
